102fe6c28a67c169adc90f850d01fdc2e554419d
Jorge L <jorge.alfmur@gmail.com>
2019-04-27 21:32:09 -0500

    setup prisma into context

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 const Query = require('./resolvers/query')
 const Mutation = require('./resolvers/mutation')
 const User = require('./resolvers/user')
@@ -14,6 +15,9 @@ const resolvers = {
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
+    context: {
+        prisma,
+    },
 })
 
 server.start(() => console.log(`server is running on http://localhost:4000 🚀`))
