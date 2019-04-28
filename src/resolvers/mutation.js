147f2153e79f5c68c10122be07df0934b8ba2d30
Jorge L <jorge.alfmur@gmail.com>
2019-04-28 13:37:58 -0500

    some little fixes

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 
-const signUp = (_, args, ctx) => {
+const signUp = async (_, args, ctx) => {
     const password = await bcrypt.hash(args.password, 10)
     return ctx.prisma.createUser({ ...args, password })
 }
@@ -9,7 +9,9 @@ const createDraft = (_, args, ctx) => {
     return ctx.prisma.createPost({
         title: args.title,
         author: {
-            connect: args.userId,
+            connect: {
+                id: args.userId
+            }
         },
     })
 }
